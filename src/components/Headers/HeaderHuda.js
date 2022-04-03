723be31a60a2580473d315b8361c538c3b89d765
ayogatot <azerino25@gmail.com>
2022-04-03 18:41:20 +0700

    remove unused code

--- src/components/Headers/HeaderHuda.js
+++ src/components/Headers/HeaderHuda.js
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // import { collection, onSnapshot } from 'firebase/firestore'
 // import { firestore } from "config";
-
-
-import CardStats from "components/Cards/CardStats.js";
 
 export default function HeaderStats() {
   
